62f652b9e2516144807f13965f0aaf2941680eae
Pranay <pranaythombre05@gmail.com>
2025-10-07 15:17:47 +0530

    toast changed

--- Frontend/Job_Protel/src/main.jsx
+++ Frontend/Job_Protel/src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import store from './redux/store.js'
 import { Provider } from 'react-redux'
-import { Toaster } from './components/ui/sonner.jsx'
+import { Toaster } from './Components/ui/sonner.jsx'
 import { PersistGate } from 'redux-persist/integration/react'
 import { persistStore } from 'redux-persist'
 
